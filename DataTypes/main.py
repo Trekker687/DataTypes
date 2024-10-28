7004583ba31c29ac1b7bfb123a05bfb0ee318e63
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-10-28 17:28:38 +0530

    Add files via upload

---
+++ DataTypes/main.py
@@ -0,0 +1,15 @@
+name = "Manvir"
+print(name)
+print(type(name))
+
+age = 300
+print(age)
+print(type(age))
+
+weight = 40.1256921258731284832
+print("weight")
+print(type(weight))
+
+very_cool = True
+print(very_cool)
+print(type(very_cool))
